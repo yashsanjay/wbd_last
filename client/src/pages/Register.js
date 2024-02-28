3cc24b0fcb7936a83a61ea6fe9b2a372024a916d
poorvitha7 <129936261+poorvitha7@users.noreply.github.com>
2024-02-29 00:42:22 +0530

    Confirm password

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,15 +5,28 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+
 const Register = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  //form handler
-  const onfinishHandler = async (values) => {
+  const [form] = Form.useForm(); // Add this line to get access to the form instance
+
+  // form handler
+  const onFinishHandler = async (values) => {
     try {
       dispatch(showLoading());
+
+      // Additional validation for password and confirm password
+      if (values.password !== values.confirmPassword) {
+        dispatch(hideLoading());
+        message.error("Passwords do not match");
+        return;
+      }
+
       const res = await axios.post("/api/v1/user/register", values);
       dispatch(hideLoading());
+
       if (res.data.success) {
         message.success("Register Successfully!");
         navigate("/login");
@@ -22,39 +35,113 @@ const Register = () => {
       }
     } catch (error) {
       dispatch(hideLoading());
-      console.log(error);
+      console.error(error);
       message.error("Something Went Wrong");
     }
   };
+
   return (
     <>
-      <div className="form-container ">
+      <div className="form-container">
         <Form
+          form={form} // Add this line to pass the form instance
           layout="vertical"
-          onFinish={onfinishHandler}
+          onFinish={onFinishHandler}
           className="register-form"
         >
-          <h3 className="text-center">Register From</h3>
+         
+
+          <h3 className="text-center">Register Form</h3>
           <Form.Item label="Name" name="name">
             <Input type="text" required />
           </Form.Item>
-          <Form.Item label="Email" name="email">
-            <Input type="email" required />
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: 'Please input your email!' },
+              {
+                type: 'email',
+                message: 'Please enter a valid email address.',
+              },
+            ]}
+          >
+            <Input type="email" />
           </Form.Item>
-          <Form.Item label="Phone" name="phone">
-            <Input type="number" required />
+
+          <Form.Item
+            label="Phone"
+            name="phone"
+            rules={[
+              { required: true, message: 'Please input your phone number!' },
+              {
+                pattern: /^[1-9]\d{9}$/, // Add your phone number regex pattern here
+                message: 'Please enter a valid 10-digit phone number.',
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Form.Item label="District" name="district">
-            <Input type="text" required />
+
+          <Form.Item
+            label="Age"
+            name="age"
+            rules={[
+              { required: true, message: 'Please input your age!' },
+              {
+                pattern: /^(0?[1-9]|[1-9][0-9])$/, // Add your age regex pattern here
+                message: 'Please enter a valid age.',
+              },
+            ]}
+          >
+            <Input type="number" />
           </Form.Item>
-          <Form.Item label="Age" name="age">
-            <Input type="number" required />
+          <Form.Item
+            label="District"
+            name="district"
+            rules={[{ required: true, message: 'Please input your district!' }]}
+          >
+            <Input type="text" />
           </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input type="password" required />
+          <Form.Item
+            label="Password"
+            name="password"
+            hasFeedback
+            rules={[
+              { required: true, message: 'Please input your password!' },
+              {
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, // Add your password regex pattern here
+                message: 'Password must contain at least 8 characters, including letters and numbers.',
+              },
+            ]}
+          >
+            <Input.Password
+              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
           </Form.Item>
+
+          <Form.Item
+  label="Confirm Password"
+  name="confirmPassword"
+  hasFeedback
+  rules={[
+    { required: true, message: 'Please confirm your password!' },
+    ({ getFieldValue }) => ({
+      validator(_, value) {
+        if (!value || getFieldValue('password') === value) {
+          return Promise.resolve();
+        }
+        return Promise.reject('The two passwords do not match!');
+      },
+    }),
+  ]}
+>
+  <Input.Password
+    iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+  />
+</Form.Item>
           <Link to="/login" className="m-2">
-            Already user login here
+            Already a user? Login here
           </Link>
           <button className="btn btn-primary" type="submit">
             Register
